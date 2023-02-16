dedb0f79c4e352048897a8fdd19fda16109f6f60
Abd Subhan Khan <subhan13j1@gmail.com>
2023-02-16 20:18:23 +0400

    add config for env variables

---
+++ backend/src/utils/config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  port: process.env.PORT || 8080,
+  db: process.env.DB_URI,
+  // db: 'mongodb+srv://dbUser:10BGMICOyuXYiSI8@cluster0.iudkq.mongodb.net/?retryWrites=true&w=majority'
+};
